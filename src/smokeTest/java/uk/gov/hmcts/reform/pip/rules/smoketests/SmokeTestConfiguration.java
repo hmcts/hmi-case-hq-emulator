c30f9128341217e2a1d872ca1414ee141ac2d504
Christopher Pateman <christopher.pateman@version1.com>
2021-09-27 12:08:40 +0100

    remove dummy test

--- src/smokeTest/java/uk/gov/hmcts/reform/pip/rules/smoketests/SmokeTestConfiguration.java
+++ src/smokeTest/java/uk/gov/hmcts/reform/pip/rules/smoketests/SmokeTestConfiguration.java
@@ -3,16 +3,8 @@ package uk.gov.hmcts.reform.pip.rules.smoketests;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
 
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 @ComponentScan("uk.gov.hmcts.reform.pip.rules.smoketests")
 @PropertySource("application.properties")
 public class SmokeTestConfiguration {
-
-  @Test
-  public void exampleOfTest() {
-      assertTrue(System.currentTimeMillis() > 0, "Example of Unit Test");
-  }
 
 }
